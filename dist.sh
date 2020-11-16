187684cfe4afe39e966ba52712e20eb8477258a9
Manuel Jimenez <manuel.jimenez@adevinta.com>
2020-11-16 10:03:17 +0100

    Better file copies

--- dist.sh
+++ dist.sh
@@ -3,8 +3,7 @@ distDir=./build/dist
 ./gradlew shadowJar
 mkdir -p $distDir
 rm -r $distDir/*
-cp start.sh $distDir
-cp start.bat $distDir
+cp start.{sh,bat} $distDir
 mv build/libs/shadow-1.0-SNAPSHOT-all.jar $distDir/program.jar
 chmod +x $distDir/start.sh
 cd $distDir
